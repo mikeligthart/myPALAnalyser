b6abd35eb043f084b1362fd716217cb783000f6e
Mike Ligthart <mike.ligthart@gmail.com>
2016-04-17 21:24:54 +0200

    adds more details to the content timeline

--- plotContentTimelines.py
+++ plotContentTimelines.py
@@ -9,9 +9,10 @@ import math
 import csv
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 # Settings
-debug = False
+debug = True
 save_as = 'png'
 number_of_days = 23
 
@@ -19,6 +20,7 @@ number_of_days = 23
 login_time = np.load('data/cleaned/login_time.npy')
 average_nr_of_words = np.load('data/cleaned/average_nr_of_words.npy')
 personal_score = np.load('data/cleaned/personal_score.npy')
+goal_difficulity = np.load('data/cleaned/goal_difficulty.npy')
 
 participants_CSV = 'data/cleaned/participants.csv'
 with open(participants_CSV, 'rb') as csv_file:
@@ -42,6 +44,12 @@ def get_yticks(y, nr_of_steps):
 
 # Build plots using matplotlib
 bar_width = 0.35
+font_prop = FontProperties()
+font_prop.set_size('x-small')
+
+#Red and blue line to indicate the evaluation session and the half way point    
+stop_data=[21, 20, 14, 19, 20, 19, 24, 19, 20, 14, 20, 16, 21, 20]
+half_way=[8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9]
 
 numberOfParticipants = len(participants)
 if (debug):
@@ -59,43 +67,95 @@ for participant_index in range(0, numberOfParticipants):
     dpifig = 300
     
     #Login Time
+    #Data processing
     y = login_time[participant_index, 0:number_of_days]/60
+    median = np.median(y[np.nonzero(y)])
+    iqr = np.subtract(*np.percentile(y[np.nonzero(y)], [75, 25]))    
+    total = np.sum(y)
+    text = 'median = ' + str(median) + '\nIQR = ' + str(iqr) + '\nTotal = ' + str(total)
+        
+    #Plot processing
     ax0.bar(temp_x, y, width=bar_width)
     ax0.set_title('Login time')
     y_lbl0 = ax0.set_ylabel("Minutes")
     y_ticks = get_yticks(y, 4)
     ax0.set_yticks(y_ticks)
     ax0.set_yticklabels(y_ticks, size=7)
+    ax0.axvline(stop_data[participant_index], color='r')
+    ax0.axvline(half_way[participant_index], color='b')
+    metrics0 = f.text(1.005, 0.84, text, size=7)
 
     #Number of words
     y = average_nr_of_words[participant_index, 0:number_of_days]
+    median = np.median(y[np.nonzero(y)])
+    iqr = np.subtract(*np.percentile(y[np.nonzero(y)], [75, 25]))    
+    total = np.sum(y)
+    text = 'median = ' + str(median) + '\nIQR = ' + str(iqr) + '\nTotal = ' + str(total)
+    
     ax1.bar(temp_x, y, width=bar_width)
     ax1.set_title('Average number of words')    
     y_lbl1 = ax1.set_ylabel("# words")
     y_ticks = get_yticks(y, 4)
     ax1.set_yticks(y_ticks)
     ax1.set_yticklabels(y_ticks, size=7)
+    ax1.axvline(stop_data[participant_index], color='r')
+    ax1.axvline(half_way[participant_index], color='b')
+    metrics1 = f.text(1.005, 0.60, text, size=7)
     
     #Personal level
     y = personal_score[participant_index, 0:number_of_days]
+    median = np.median(y[np.nonzero(y)])
+    iqr = np.subtract(*np.percentile(y[np.nonzero(y)], [75, 25]))    
+    total = np.sum(y)
+    text = 'median = ' + str(median) + '\nIQR = ' + str(iqr) + '\nTotal = ' + str(total)
+    
     ax2.bar(temp_x, y, width=bar_width)
     ax2.set_title('Personal level of added content')
     y_lbl2 = ax2.set_ylabel("Personal level")
     y_ticks = [0, 1, 2, 3, 4]
     ax2.set_yticks(y_ticks)
     ax2.set_yticklabels(y_ticks, size=7)
+    ax2.axvline(stop_data[participant_index], color='r')
+    ax2.axvline(half_way[participant_index], color='b')
+    metrics2 = f.text(1.005, 0.38, text, size=7)
 
     #Goal dificultiy
-    ax3.bar(temp_x, np.zeros(23), width=bar_width)
+    y_daily = goal_difficulity[participant_index, 0:number_of_days, 0]
+    median_daily = np.median(y_daily[np.nonzero(y_daily)])
+    iqr_daily = np.subtract(*np.percentile(y_daily[np.nonzero(y_daily)], [75, 25]))    
+    total_daily = np.sum(y_daily)
+    
+    y_total = goal_difficulity[participant_index, 0:number_of_days, 1]
+    y_total_nonzero = y_total[np.nonzero(y_total)]
+    if len(y_total_nonzero) > 0:
+        median_total = np.median(y_total_nonzero)
+        iqr_total = np.subtract(*np.percentile(y_total_nonzero, [75, 25]))
+    else:
+        median_total = 0
+        iqr_total = 0
+    total_total = np.sum(y_total)
+    total_combined = total_daily + total_total
+    text = '    Daily Goal    Total Goal \nmedian     ' + str(median_daily) + '    '+ str(median_total) +' \nIQR    '+ str(iqr_daily) +' \t ' + str(iqr_total) + ' \ntotal ' + str(total_daily) + ' \t '+ str(total_total)
+    
+    y_sum = np.sum(goal_difficulity[participant_index, 0:number_of_days, :], axis=1)
+    ax3.bar(temp_x, y_daily, width=bar_width, color='b', label='Daily Goals')
+    ax3.bar(temp_x, y_total, width=bar_width, bottom=y_daily, color='r', label='Total Goals')
     ax3.set_title('Goal difficulty')
     y_lbl3 = ax3.set_ylabel("Difficulty")
     ax3.set_xlabel("Date")
+    y_ticks = get_yticks(y_sum, 4)
+    ax3.set_yticks(y_ticks)
+    ax3.set_yticklabels(y_ticks, size=7)
+    ax3.axvline(stop_data[participant_index], color='r')
+    ax3.axvline(half_way[participant_index], color='b')
+    lgd3 = ax3.legend(prop = font_prop, loc='upper right', bbox_to_anchor=(1.4, 1.25), title='Goals')
+    metrics3 = f.text(1.005, 0.15, text, size=7)
 
     f.tight_layout()
     
     if save_as == "png" or save_as == "both":
-        plt.savefig('data/cleaned/timelines/content/content_timeline_p' + str(participant_index + 1) + '.png', bbox_extra_artists=(ttl, y_lbl0, y_lbl1, y_lbl2, y_lbl3, ), bbox_inches='tight', dpi=dpifig)
+        plt.savefig('data/cleaned/timelines/content/content_timeline_p' + str(participant_index + 1) + '.png', bbox_extra_artists=(ttl, y_lbl0, y_lbl1, y_lbl2, y_lbl3, lgd3, metrics0, metrics1, metrics2, metrics3, ), bbox_inches='tight', dpi=dpifig)
     if save_as == "svg" or save_as == "both":
-        plt.savefig('data/cleaned/timelines/content/content_timeline_p' + str(participant_index + 1) + '.svg', bbox_extra_artists=(ttl, y_lbl0, y_lbl1, y_lbl2, y_lbl3, ), bbox_inches='tight', format='svg')
+        plt.savefig('data/cleaned/timelines/content/content_timeline_p' + str(participant_index + 1) + '.svg', bbox_extra_artists=(ttl, y_lbl0, y_lbl1, y_lbl2, y_lbl3, lgd3, metrics0, metrics1, metrics2, metrics3, ), bbox_inches='tight', format='svg')
     plt.close(f)
 
